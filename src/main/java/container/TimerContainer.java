2c77408502f05b38feac2eab9a5847e7189ce628
Leanka <comamydzisiajnasniadanie@gmail.com>
2018-08-18 22:39:12 +0200

    Create timer container class frame

--- src/main/java/container/TimerContainer.java
+++ src/main/java/container/TimerContainer.java
@@ -1,4 +1,29 @@
 package container;
 
-public class TimerContainer {
+import model.Timer;
+import java.util.HashMap;
+import java.util.Map;
+
+public class TimerContainer implements Container{
+    Map <String, Timer> timers = new HashMap <>();
+
+    @Override
+    public void addTimer(Timer timer) {
+
+    }
+
+    @Override
+    public Timer getTimer(String name) {
+        return null;
+    }
+
+    @Override
+    public void closeAllTimers() {
+
+    }
+
+    @Override
+    public boolean isNameAvailable(String name) {
+        return false;
+    }
 }
