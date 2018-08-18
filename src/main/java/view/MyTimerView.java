0eb6834e2b4b9ab3c41f82aa42821cbbe5585119
Leanka <comamydzisiajnasniadanie@gmail.com>
2018-08-19 00:41:22 +0200

    Implement getting user input, it's validation, printing timer

--- src/main/java/view/MyTimerView.java
+++ src/main/java/view/MyTimerView.java
@@ -1,4 +1,37 @@
 package view;
 
-public class MyTimerView {
+import model.Timer;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class MyTimerView implements TimerView {
+
+    @Override
+    public String[] getInput() {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String input = "";
+        String [] inputWords;
+
+        try {
+            input = reader.readLine().replaceAll("\\s+", " ").trim();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("input: " + input);
+        if(input.isEmpty()){
+            inputWords = new String[0];
+        }else {
+            inputWords = input.split("\\s");
+        }
+
+        return inputWords;
+    }
+
+    @Override
+    public void printTimer(Timer timer) {
+        System.out.println(timer);
+    }
 }
