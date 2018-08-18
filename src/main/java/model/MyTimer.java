89fa36f71a9456ea47a061a78bf3c9d308866a7c
Leanka <comamydzisiajnasniadanie@gmail.com>
2018-08-18 14:57:36 +0200

    Create custom timers class frame

--- src/main/java/model/MyTimer.java
+++ src/main/java/model/MyTimer.java
@@ -1,4 +1,15 @@
 package model;
 
-public class MyTimer {
+public class MyTimer extends Thread implements Timer{
+    private final long startTime = System.currentTimeMillis();
+    private long endTime = 0;
+    private long startPause;
+    private long totalPauseCountInMillis = 0;
+    private volatile boolean isThreadSuspended = false;
+
+    public MyTimer(String name) {
+        super(name);
+    }
+
+
 }
