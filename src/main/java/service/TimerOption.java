e8375264d5755e29319f3d5d25fc7cd71bf11bba
Leanka <comamydzisiajnasniadanie@gmail.com>
2018-08-19 00:49:55 +0200

    Create interface covering timers options

--- src/main/java/service/TimerOption.java
+++ src/main/java/service/TimerOption.java
@@ -1,4 +1,10 @@
 package service;
 
 public interface TimerOption {
+    void showAllTimers();
+    void showTimer(String name);
+    void startTimer(String name);
+    void stopTimer(String string);
+    void pauseTimer(String name);
+    void restartTimer(String name);
 }
