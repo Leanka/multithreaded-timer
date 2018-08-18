c7466d524bde154bba79b0a29f8a8de65d6cf556
Leanka <comamydzisiajnasniadanie@gmail.com>
2018-08-18 21:56:54 +0200

    Implement starting timer

--- src/main/java/controller/MyTimerController.java
+++ src/main/java/controller/MyTimerController.java
@@ -3,9 +3,9 @@ package controller;
 import model.Timer;
 
 public class MyTimerController implements TimerController {
-    
-    public void startTimer(Timer timer) {
 
+    public void startTimer(Timer timer) {
+        timer.start();
     }
 
     public void stopTimer(Timer timer) {
