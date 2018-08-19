c75762483fcfdf9e6f405b46c688f16080d8daf5
Leanka <comamydzisiajnasniadanie@gmail.com>
2018-08-19 16:17:28 +0200

    Create timer service class frame

--- src/main/java/service/MyTimerService.java
+++ src/main/java/service/MyTimerService.java
@@ -1,4 +1,14 @@
 package service;
 
-public class MyTimerService {
+public class MyTimerService implements TimerService{
+    private TimerOption option;
+
+    public MyTimerService(TimerOption option) {
+        this.option = option;
+    }
+
+    @Override
+    public void runTimerService() {
+
+    }
 }
