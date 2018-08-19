a3d586979c1d567a9225b7a7234d52877068e863
Leanka <comamydzisiajnasniadanie@gmail.com>
2018-08-19 20:44:22 +0200

    Implement starting app in main

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,5 +1,20 @@
+import container.TimerContainer;
+import controller.MyTimerController;
+import service.MyTimerOption;
+import service.MyTimerService;
+import service.TimerService;
+import view.MyInput;
+import view.MyView;
+
 public class Application {
     public static void main(String[] args) {
-
+        TimerService timerService = new MyTimerService(
+                new MyTimerOption(
+                        new MyTimerController(),
+                        new TimerContainer(),
+                        new MyView()),
+                new MyInput(),
+                new MyView());
+        timerService.runTimerService();
     }
 }
