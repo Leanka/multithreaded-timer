0cbd32eb0c69bacc5f0bd71c9f6438b30e9e5e7e
Leanka <comamydzisiajnasniadanie@gmail.com>
2018-08-19 14:46:16 +0200

    Create timer options class frame

--- src/main/java/service/MyTimerOption.java
+++ src/main/java/service/MyTimerOption.java
@@ -1,4 +1,52 @@
 package service;
 
-public class MyTimerOption {
+import container.Container;
+import controller.TimerController;
+import view.TimerView;
+
+public class MyTimerOption implements TimerOption{
+    private TimerController controller;
+    private Container container;
+    private TimerView view;
+
+    public MyTimerOption(TimerController controller, Container timerContainer, TimerView view) {
+        this.controller = controller;
+        this.container = timerContainer;
+        this.view = view;
+    }
+
+    @Override
+    public void showAllTimers() {
+
+    }
+
+    @Override
+    public void showTimer(String name) {
+
+    }
+
+    @Override
+    public void startTimer(String name) {
+
+    }
+
+    @Override
+    public void stopTimer(String string) {
+
+    }
+
+    @Override
+    public void pauseTimer(String name) {
+
+    }
+
+    @Override
+    public void restartTimer(String name) {
+
+    }
+
+    @Override
+    public void terminateTimers() {
+        container.terminateAllTimers();
+    }
 }
